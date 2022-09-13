c724a7710dda925faa1fe505ec225d1adbbff64e
Vaibhav <vaibhavshukla2704@gmail.com>
2022-09-14 00:27:46 +0530

    working on authentication

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import { Home, Login } from "./components";
+
+const App = () => {
+  const [auth, setAuth] = useState(
+    false || window.localStorage.getItem("auth") === "true"
+  );
+
+  return (
+    <div className="w-screen h-screen bg-primary flex justify-center items-center">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
