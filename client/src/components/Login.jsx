126b364cbf5695df02cea99281ae2fd7e6801c15
Vaibhav <vaibhavshukla2704@gmail.com>
2022-09-14 14:53:46 +0530

    authentication in server side is completed

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,8 +3,9 @@ import { FcGoogle } from "react-icons/fc";
 import { app } from "../config/firebase.config";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-const Login = () => {
+const Login = ({ setAuth }) => {
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
@@ -12,10 +13,30 @@ const Login = () => {
 
   const loginWithGoogle = async () => {
     await signInWithPopup(firebaseAuth, provider).then((userCred) => {
-      console.log(userCred);
+      if (userCred) {
+        setAuth(true);
+        window.localStorage.setItem("auth", "true");
+
+        firebaseAuth.onAuthStateChanged((userCred) => {
+          if (userCred) {
+            userCred.getIdToken().then((token) => {
+              console.log(token);
+            });
+            navigate("/", { replace: true });
+          } else {
+            setAuth(false);
+            navigate("/login ");
+          }
+        });
+      }
     });
   };
 
+  useEffect(() => {
+    if (window.localStorage.getItem("auth") === "true") {
+      navigate("/", { replace: true });
+    }
+  }, []);
   return (
     <div className="relative w-screen h-screen">
       <div className="absolute inset-0 bg-darkOverlay flex items-center justify-center p-4">
