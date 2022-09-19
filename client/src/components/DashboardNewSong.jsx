2fef3755acdfe79e585eb03e7d804c7977588f2b
Vaibhav <vaibhavshukla2704@gmail.com>
2022-09-19 08:54:53 +0530

    filter button work in new song completed

--- client/src/components/DashboardNewSong.jsx
+++ client/src/components/DashboardNewSong.jsx
@@ -13,10 +13,53 @@ import { MdDelete } from "react-icons/md";
 
 import { storage } from "../config/firebase.config";
 import { useStateValue } from "../context/StateProvider";
-import { FilterButtons } from "./FilterButtons";
+import { actionType } from "../context/reducer";
+import { FilterButtons } from "./index";
+import { filters, filterByLanguage } from "../utils/supportfunctions";
+import { getAllAlbums, getAllArtists } from "../api";
 
 const DashboardNewSong = () => {
-  return <div>DashboardNewSong</div>;
+  const [songName, setSongName] = useState("");
+  const [{ allArtists, allAlbums }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    if (!allArtists) {
+      getAllArtists().then((data) => {
+        dispatch({
+          type: actionType.SET_ALL_ARTISTS,
+          allArtists: data.artists,
+        });
+      });
+    }
+
+    if (!allAlbums) {
+      getAllAlbums().then((data) => {
+        dispatch({
+          type: actionType.SET_ALL_ALBUMS,
+          allAlbums: data.albums,
+        });
+      });
+    }
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center jusitfy-center p-4 border border-gray-300 gap-4 rounded-md">
+      <input
+        type="text"
+        placeholder="Type your Song Name....."
+        className="w-full p-3 rounded-md text-base font-semibold text-textColor outline-none shadow-sm hover:shadow-md hover:border-gray-500 border border-gray-300 bg-transparent"
+        value={songName}
+        onChange={(e) => setSongName(e.target.value)}
+      />
+
+      <div className="flex w-full justify-between flex-wrap items-center gap-4">
+        <FilterButtons filterData={allArtists} flag={"Artist"} />
+        <FilterButtons filterData={allAlbums} flag={"Album"} />
+        <FilterButtons filterData={filterByLanguage} flag={"Language"} />
+        <FilterButtons filterData={filters} flag={"Category"} />
+      </div>
+    </div>
+  );
 };
 
 export default DashboardNewSong;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -60,5 +60,8 @@ module.exports = {
       },
     },
   },
-  // plugins: [require("tailwind-scrollbar")],
+  plugins: [
+    // ...
+    require("tailwind-scrollbar"),
+  ],
 };
